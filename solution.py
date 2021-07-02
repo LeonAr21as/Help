5e92da749c4f0dece41438951354e7b9d090e760
LeonAr21as <86841222+LeonAr21as@users.noreply.github.com>
2021-07-02 11:14:27 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -2,11 +2,12 @@ import csv
 from datetime import datetime
 from math import sqrt, pi, cos, log10
 
+
+
 class Solution:
     
     def __init__(self, filename):
         # id,first_name,last_name,dateOfBirth,interests,latitude,longitude
-        filename = "data.cvs"
         self.students = []
         with open(filename) as file:
             reader = csv.reader(file, delimiter=',', skipinitialspace=True)
@@ -22,28 +23,20 @@ class Solution:
 
                 i += 1
 
+                
 
     # Distancia entre estudiantes
-    def Prob1(self, id1, id2):
-      la = Solution().students[id1][3] * pi / 180
-      lo = Solution().students[id1][4] * pi / 180
-      d = 6371.009*sqrt(abs(la-lo)^2 + (abs(la-lo)*cos(sum(la-lo)/2))^2)
-      return d
+    def ejercicio1(self, id1, id2):
+        # Escribe tu solucion aqui
+
+        return #tu resultado
+
 
     # Compatibilidad entre estudiantes
     def ejercicio2(self, id1, id2):
-        AgeD = abs(id1[4]-id2[4])
-        if AgeD <=1 :
-            return 1
-        if AgeD >= 1:
-            return (AgeD)^(-1)
-        LID1 = list(id1[3])
-        LID2 = list(id2[3])
-        COIN = len(LID1 & LID2)
-        CP = ((log10*"ejercicio1"))^(-1) + AgeD + COIN
+        # Escribe tu solucion aqui
 
-        return CP
-
+        return #tu resultado
 
     # Estudiantes mas cercanos
     def ejercicio3(self, id, n):
@@ -71,3 +64,6 @@ class Solution:
         # Escribe tu solucion aqui
 
         return #tu resultado
+
+
+
